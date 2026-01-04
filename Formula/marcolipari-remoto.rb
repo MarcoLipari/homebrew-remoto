39308106c2c63ca7f080d4659a5472f587a22974
MarcoLipari <mlipari04@icloud.com>
2026-01-04 15:34:38 -0500

    Replaced head with SHA

--- Formula/marcolipari-remoto.rb
+++ Formula/marcolipari-remoto.rb
@@ -1,7 +1,8 @@
 class MarcolipariRemoto < Formula
   desc "Voice-controlled remote computer access system"
   homepage "https://github.com/MarcoLipari/Remoto-Mac"
-  head "https://github.com/MarcoLipari/Remoto-Mac.git", branch: "main"
+  url "https://github.com/marcolipari/remoto-mac/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "b2c3f199703497d7a8ad6bcc0007d3bc1d517eed5e8f30725ecbe0bcbb5945c6"
   depends_on "cloudflare/cloudflare/cloudflared"
   depends_on "ffmpeg"
   depends_on "mediamtx"
